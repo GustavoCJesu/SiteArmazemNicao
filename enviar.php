bb82864284e5efa2750615f5c4cd4ab840c7fdbf
GustavoJesuino <gustavo.jesuino1129@gmail.com>
2026-03-09 14:34:27 -0300

    Alterações
    - BackgroundColor(serviços)
    - Botão fixo de whatsapp em todas as paginas

--- enviar.php
+++ enviar.php
@@ -4,37 +4,51 @@ require "vendor/autoload.php";
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
-$name    = $_POST["name"];
-$email   = $_POST["email"];
-$message = $_POST["message"];
-$telefone = $_POST["telefone"];
+// Coleta de dados
+$name       = $_POST["name"];
+$email      = $_POST["email"];
+$message    = $_POST["message"];
+$telefone   = $_POST["telefone"];
+$assunto    = $_POST["assunto"];
 
 $mail = new PHPMailer(true);
-$mail->isSMTP();
-$mail->CharSet = "UTF-8";
 
-$mail->Host       = 'smtp.gmail.com';
-$mail->SMTPAuth   = true;
-$mail->Username   = 'gustavocontijesuino4@gmail.com';
-$mail->Password   = 'mccu qlmv waho rbak';
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port       = 587;
+try {
+    // Configurações do Servidor
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'gustavocontijesuino4@gmail.com';
+    $mail->Password   = 'mccu qlmv waho rbak'; // Lembre-se de manter isso em segredo!
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
 
-$mail->SMTPDebug = 2;
+    // Se quiser que o alerta apareça limpo, recomendo desativar o Debug (0)
+    $mail->SMTPDebug = 0; 
 
-$mail->setFrom('gustavocontijesuino4@gmail.com', 'Site');
-$mail->addReplyTo($email, $name);
-$mail->addAddress('gustavocontijesuino4@gmail.com');
+    // Destinatários
+    $mail->setFrom('gustavocontijesuino4@gmail.com', 'Site');
+    $mail->addReplyTo($email, $name);
+    $mail->addAddress('gustavocontijesuino4@gmail.com');
 
-$mail->isHTML(true);
-$mail->Subject = 'Email do Site';
-$mail->Body    = $name."<br>".$email."<br>".$telefone."<br>".nl2br($message);
-$mail->AltBody = $message;
+    // Conteúdo
+    $mail->isHTML(true);
+    $mail->Subject = 'Email do Site';
+    $mail->Body    = "<b>Nome:</b> $name <br> <b>E-mail:</b> $email <br> <b>Telefone:</b> $telefone <br><br> <b>Mensagem:</b><br>" . nl2br($message);
+    $mail->AltBody = $message;
 
-$mail->send();
+    // Envio
+    if ($mail->send()) {
+        echo "<script>
+                alert('Mensagem enviada com sucesso!');
+                window.location.href = 'index.html'; // Redireciona de volta para o formulário
+              </script>";
+    }
 
-if (!$mail->send()) {
-    echo "Erro: " . $mail->ErrorInfo;
-} else {
-    echo "Mensagem enviada com sucesso!";
+} catch (Exception $e) {
+    echo "<script>
+            alert('Erro ao enviar: {$mail->ErrorInfo}');
+            window.history.back(); // Volta para a página anterior
+          </script>";
 }
